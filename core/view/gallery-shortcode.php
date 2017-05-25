b58f1ff9f7cd983b91f31151f92e812e8f266477
Hamed Moodi <hamedmoodi2011@gmail.com>
2017-05-25 23:28:06 +0430

    fix bug in html and css

--- core/view/gallery-shortcode.php
+++ core/view/gallery-shortcode.php
@@ -5,5 +5,5 @@
 		    <img alt="<?php echo esc_attr( get_the_title( $aid ) );?>" class="colorbox-image" src="<?php echo wp_get_attachment_thumb_url( $aid );?>">
 		</a>
 	<?php endforeach;?>
-<div>
+</div>
 <div style="clear: both;"></div>
