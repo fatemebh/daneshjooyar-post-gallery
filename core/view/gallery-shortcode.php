a12a207f48679216da61846150224789185dc896
Hamed Moodi <hamedmoodi2011@gmail.com>
2017-06-25 04:16:04 +0430

    edit some css

--- core/view/gallery-shortcode.php
+++ core/view/gallery-shortcode.php
@@ -3,7 +3,7 @@
 	<?php foreach( $dy_post_gallery_images as $aid ):?>
 		<?php if( wp_attachment_is_image( $aid ) ):?>
 			<a rel="swipebox-gallery" href="<?php echo wp_get_attachment_url( $aid );?>" class="swipebox" title="<?php echo esc_attr( get_the_title( $aid ) );?>">
-				<img src="<?php echo wp_get_attachment_thumb_url( $aid );?>" alt="title="<?php echo esc_attr( get_the_title( $aid ) );?>"">
+				<img src="<?php echo wp_get_attachment_thumb_url( $aid );?>" alt="<?php echo esc_attr( get_the_title( $aid ) );?>">
 			</a>
 		<?php elseif( wp_attachment_is( 'video', $aid ) ):?>
 			<a rel="swipebox-gallery" href="<?php echo wp_get_attachment_url( $aid );?>" class="swipebox swipebox-video" title="<?php echo esc_attr( get_the_title( $aid ) );?>">
--- dy-post-gallery.php
+++ dy-post-gallery.php
@@ -5,7 +5,7 @@
  * Author: Hamed Moodi
  * Author URI: http://ircodex.ir
  * Description: this is simple plugin for add gallery for any post such as product gallery
- * Version: 1.0.1
+ * Version: 1.0.2
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: daneshjooyar-post-gallery
